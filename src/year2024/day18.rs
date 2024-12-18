4a0fd76e38ad24ab0c206ecb69643481122e8206
mokiros <mokiros@mokiros.dev>
2024-12-18 13:34:24 +0300

    [Day 18] Now with binary search, also added some comments

--- src/year2024/day18.rs
+++ src/year2024/day18.rs
@@ -1,14 +1,15 @@
-use std::{
-	collections::VecDeque,
-	io::{BufRead, BufReader, Lines},
-};
+use std::{collections::VecDeque, io::BufRead};
 
 use crate::util::{direction::Direction, matrix::Matrix, position::Position};
 
+// Input does not contain map size, so we explicitly state it
 const WIDTH: usize = 71;
 const HEIGHT: usize = 71;
 
-fn bfs(map: &Matrix<bool>) -> Option<usize> {
+// Simple breadth first search
+// Map contains values, indicating when the byte would fall
+// so if we need to check at 1024th byte, we consider everything below 1024 as a wall
+fn bfs(map: &Matrix<u16>, iteration: u16) -> Option<usize> {
 	let start_position = Position { x: 0, y: 0 };
 	let end_position = Position {
 		x: WIDTH as isize - 1,
@@ -31,9 +32,10 @@ fn bfs(map: &Matrix<bool>) -> Option<usize> {
 		for direction in Direction::all() {
 			let next_pos = direction.update_position(pos);
 
-			if !map.get(next_pos.x, next_pos.y).unwrap_or(true)
-				&& !visited.get(next_pos.x, next_pos.y).unwrap_or(true)
-			{
+			let is_wall = map.get(next_pos.x, next_pos.y).unwrap_or(0) < iteration;
+			let is_visited = visited.get(next_pos.x, next_pos.y).unwrap_or(true);
+
+			if !is_wall && !is_visited {
 				visited.set(next_pos.x, next_pos.y, true);
 				costs.set(next_pos.x, next_pos.y, costs.get(pos.x, pos.y).unwrap() + 1);
 				queue.push_front(next_pos);
@@ -45,11 +47,13 @@ fn bfs(map: &Matrix<bool>) -> Option<usize> {
 }
 
 pub fn solve<R: BufRead>(reader: R) -> (i64, i64) {
-	const CALCULATE_AT: usize = 1023;
-	let mut map: Matrix<bool> = Matrix::new(WIDTH as u8, HEIGHT as u8, vec![false; WIDTH * HEIGHT]);
+	let mut map: Matrix<u16> =
+		Matrix::new(WIDTH as u8, HEIGHT as u8, vec![u16::MAX; WIDTH * HEIGHT]);
+	let mut bytes = Vec::new();
 
-	let mut p1 = 0;
+	let mut max: u16 = 0;
 
+	// Read all positions at once
 	for (i, line) in reader.lines().enumerate() {
 		let line = line.unwrap();
 		let mut parts = line.split(",");
@@ -57,19 +61,28 @@ pub fn solve<R: BufRead>(reader: R) -> (i64, i64) {
 		let x = parts.next().unwrap().parse().unwrap();
 		let y = parts.next().unwrap().parse().unwrap();
 
-		map.set(x, y, true);
+		bytes.push(Position { x, y });
+		map.set(x, y, i as u16);
+		max = i as u16;
+	}
 
-		if i == CALCULATE_AT {
-			p1 = bfs(&map).unwrap();
-		} else if i > CALCULATE_AT {
-			let result = bfs(&map);
-			if result.is_none() {
-				println!("Part 2: {},{}", x, y);
-				println!("Index: {}", i);
-				break;
-			}
+	let p1 = bfs(&map, 1024).unwrap();
+	let mut min: u16 = 1024;
+
+	// binary search
+	while min < max {
+		let mid = (min + max) / 2;
+		if bfs(&map, mid).is_none() {
+			max = mid;
+		} else {
+			min = mid + 1;
 		}
 	}
+
+	let byte_idx = min as usize - 1;
+	let pos = bytes[byte_idx];
+
+	println!("Part 2: {},{}; Byte number: {}", pos.x, pos.y, byte_idx);
 
 	(p1 as i64, -1)
 }
