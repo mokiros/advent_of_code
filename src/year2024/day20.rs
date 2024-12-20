1d68b445573ffd800a8079398bf0932961962785
mokiros <mokiros@mokiros.dev>
2024-12-20 20:25:48 +0300

    [Day 20] Cleaned up and added comments

--- src/year2024/day20.rs
+++ src/year2024/day20.rs
@@ -5,15 +5,15 @@ use crate::util::{direction::Direction, matrix::Matrix, position::Position};
 pub fn solve<R: BufRead>(reader: R) -> (i64, i64) {
 	let (map, start_pos, end_pos) = Matrix::read_map_with_start_and_end(reader, |c| c == '#');
 
-	let mut p1 = 0;
-	let mut p2 = 0;
-
-	let mut costs: HashMap<Position, i32> = HashMap::new();
+	let mut costs: HashMap<Position, isize> = HashMap::new();
 	costs.insert(start_pos, 0);
+
 	let mut current_position = start_pos;
-	let mut step = 0;
+
+	let mut path_length = 0;
+	// Going over the path and recording the distance from the start
 	loop {
-		step += 1;
+		path_length += 1;
 		if current_position == end_pos {
 			break;
 		}
@@ -22,36 +22,42 @@ pub fn solve<R: BufRead>(reader: R) -> (i64, i64) {
 			if costs.contains_key(&next_pos) || map.get(next_pos.x, next_pos.y).unwrap_or(true) {
 				continue;
 			}
-			costs.insert(next_pos, step);
+			costs.insert(next_pos, path_length);
 			current_position = next_pos;
 			break;
 		}
 	}
 
+	// 40x40 lookup with a max distance of 20
 	let mut lookup: Vec<Position> = Vec::new();
-	let max_distance = 20;
-	for x in -max_distance..=max_distance {
-		for y in -max_distance..=max_distance {
-			let total_distance = (x as i32).abs() + (y as i32).abs();
-			if total_distance >= 2 && total_distance <= max_distance {
-				lookup.push(Position {
-					x: x as isize,
-					y: y as isize,
-				});
+	const MAX_DISTANCE: isize = 20;
+	for x in -MAX_DISTANCE..=MAX_DISTANCE {
+		for y in -MAX_DISTANCE..=MAX_DISTANCE {
+			let pos = Position { x, y };
+			let total_distance = pos.manhattan_length();
+			if total_distance >= 2 && total_distance <= MAX_DISTANCE {
+				lookup.push(pos);
 			}
 		}
 	}
 
+	let mut p1 = 0;
+	let mut p2 = 0;
+
+	const MIN_SAVE: isize = 100;
+
+	// applying the lookup above to every position on the path
 	for (pos, cost) in costs.iter() {
+		// No point in checking last 100 tiles of the path
+		if path_length - cost < MIN_SAVE {
+			continue;
+		}
 		for vec in lookup.iter() {
-			let next_pos = Position {
-				x: pos.x + vec.x,
-				y: pos.y + vec.y,
-			};
-			let vec_distance = (vec.x as i32).abs() + (vec.y as i32).abs();
+			let next_pos = pos + vec;
+			let vec_distance = vec.manhattan_length();
 			if let Some(new_cost) = costs.get(&next_pos) {
 				let diff = new_cost - cost - vec_distance;
-				if diff >= 100 {
+				if diff >= MIN_SAVE {
 					if vec_distance <= 2 {
 						p1 += 1;
 					}
