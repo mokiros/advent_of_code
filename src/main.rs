5108f6a070808d60d80d5124f85d205fc676ad18
mokiros <mokiros@mokiros.dev>
2024-12-25 19:25:55 +0300

    Day 25 (Part 1)

--- src/main.rs
+++ src/main.rs
@@ -47,6 +47,7 @@ fn run(year: u16, day: u8, input: Option<String>) -> Result<(), ()> {
 		(2024, 22) => year2024::day22::solve,
 		(2024, 23) => year2024::day23::solve,
 		(2024, 24) => year2024::day24::solve,
+		(2024, 25) => year2024::day25::solve,
 		_ => return Err(()),
 	};
 
---
+++ src/year2024/day25.rs
@@ -0,0 +1,46 @@
+use std::io::BufRead;
+
+pub fn solve<R: BufRead>(reader: R) -> (i64, i64) {
+	let mut lines = reader.lines();
+
+	let mut keys = Vec::new();
+	let mut locks = Vec::new();
+
+	while let Some(Ok(line)) = lines.next() {
+		let is_key = line.starts_with('.');
+		let mut data: [u8; 5] = [0; 5];
+		for _ in 0..5 {
+			let line = lines.next().unwrap().unwrap();
+			for (i, char) in line.chars().enumerate() {
+				if char == '#' {
+					data[i] += 1;
+				}
+			}
+		}
+		lines.next();
+		lines.next();
+
+		if is_key {
+			keys.push(data);
+		} else {
+			locks.push(data);
+		}
+	}
+
+	let mut p1 = 0;
+
+	for i in 0..keys.len() {
+		let key: [u8; 5] = keys[i];
+		'lock_loop: for j in 0..locks.len() {
+			let lock = locks[j];
+			for k in 0..5 {
+				if key[k] + lock[k] > 5 {
+					continue 'lock_loop;
+				}
+			}
+			p1 += 1;
+		}
+	}
+
+	return (p1, 0);
+}
