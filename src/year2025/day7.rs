a7fa647450bcf72970df7d4c3c5de1af30ef9963
mokiros <mokiros@mokiros.dev>
2025-12-07 09:25:41 +0300

    [Year 2025 Day 7] Minor optimizations

--- src/year2025/day7.rs
+++ src/year2025/day7.rs
@@ -1,46 +1,39 @@
-use std::{
-	collections::HashMap,
-	io::{BufRead, Seek},
-};
+use std::io::{BufRead, Seek};
 
 pub fn solve<R: BufRead + Seek>(reader: R) -> (String, String) {
 	let mut part1: u64 = 0;
 
-	let mut beams: HashMap<u64, u64> = HashMap::new();
+	let mut beams = [0_u64; 150];
 
-	let mut x = 0;
-	for byte in reader.bytes() {
-		let byte = byte.unwrap();
-
+	let mut x = 1;
+	let mut bytes = reader.bytes();
+	while let Some(Ok(byte)) = bytes.next() {
 		match byte {
 			b'S' => {
-				beams.insert(x, 1);
+				beams[x] = 1;
 			}
 			b'^' => {
-				if let Some(count) = beams.remove(&x) {
-					beams
-						.entry(x - 1)
-						.and_modify(|a| *a += count)
-						.or_insert(count);
-					beams
-						.entry(x + 1)
-						.and_modify(|a| *a += count)
-						.or_insert(count);
+				let count = beams[x];
+				if count != 0 {
+					beams[x] = 0;
+					beams[x - 1] += count;
+					beams[x + 1] += count;
 					part1 += 1;
 				}
-				x += 1;
 			}
 			b'\n' => {
-				x = 0;
+				bytes.nth(x - 1); // skip every other line
+				x = 1;
+				continue;
 			}
-			b'.' => {
-				x += 1;
-			}
+			b'.' => {}
 			byte => panic!("Invalid character: {}", byte as char),
 		}
+
+		x += 1;
 	}
 
-	let part2: u64 = beams.values().fold(0, |sum, count| sum + *count);
+	let part2: u64 = beams.iter().sum();
 
 	(part1.to_string(), part2.to_string())
 }
